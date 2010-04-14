2ae9cdecbc06faacf23c7d3c627851c52384f57b
James Levy <james@james-desktop.(none)>
2010-04-14 09:50:22 -0700

    simplejson

--- index.py
+++ index.py
@@ -11,6 +11,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
+from django.utils import simplejson
 		
 import methods 
 
